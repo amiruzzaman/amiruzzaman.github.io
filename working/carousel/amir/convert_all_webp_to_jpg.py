823debbd633766a1c9a2df25ddf29b9e4d31d128
Md Amiruzzaman <mamiruzz@kent.edu>
2025-01-14 16:25:02 -0500

    deleted webp files

--- working/carousel/amir/convert_all_webp_to_jpg.py
+++ working/carousel/amir/convert_all_webp_to_jpg.py
@@ -15,6 +15,14 @@ def convert_all_webp_in_directory(directory):
             convert_webp_to_jpg(webp_file)
             print(f"Converted {filename} to JPG.")
 
+def delete_all_webp_in_directory(directory):
+    """Deletes all WebP files in a given directory."""
+    for filename in os.listdir(directory):
+        if filename.endswith(".webp"):
+            file_path = os.path.join(directory, filename)
+            os.remove(file_path)
+            print(f"Deleted {filename}.")
 if __name__ == "__main__":
     directory = "images/Vietnam"  # Replace with your actual directory path
     convert_all_webp_in_directory(directory)
+    delete_all_webp_in_directory(directory)
